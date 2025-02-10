12695d7a7e338ac861a030c2ec7e31f2683ccb45
Fedor Krutiy <theokrutij@yandex.ru>
2025-02-10 20:54:52 +0300

    add CreateUser handler

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -1,17 +1,42 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
+
+	"github.com/theo-krutiy/minimal-go/internal/auth"
 )
 
-func (s *Server) handleGET(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "You GET it, right?")
-}
+func (s *Server) handleCreateUser() http.HandlerFunc {
+	type request struct {
+		Login    string `json:"login"`
+		Password string `json:"password"`
+	}
 
-func (s *Server) handlePOST(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "You've POSTed this:\n\n")
-	io.Copy(w, r.Body)
-	fmt.Fprintf(w, "\n\nYour address is %v", r.RemoteAddr)
+	type response struct {
+		UserId string `json:"UserId"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req request
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, "Couldn't parse request", http.StatusBadRequest)
+			return
+		}
+		newUserId, err := auth.CreateNewUser(req.Login, req.Password, s.Db)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("%v", err.Error()), http.StatusInternalServerError)
+			return
+		}
+
+		res := response{
+			UserId: newUserId,
+		}
+
+		if err := json.NewEncoder(w).Encode(res); err != nil {
+			http.Error(w, "Couldn't unmarshall response", http.StatusInternalServerError)
+			return
+		}
+	}
 }
--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,6 +1,5 @@
 package server
 
 func (s *Server) routes() {
-	s.r.HandleFunc("GET /example", s.handleGET)
-	s.r.HandleFunc("POST /example", s.handlePOST)
+	s.r.HandleFunc("POST /users", s.handleCreateUser())
 }
