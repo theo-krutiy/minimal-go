17f5d7c7f26b348cb3372cbd171571d116ccf832
Fedor Krutiy <theokrutij@yandex.ru>
2025-02-11 13:16:27 +0300

    remove salt from db, salt is merged with hash

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,6 +1,5 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     login VARCHAR(255) UNIQUE NOT NULL,
-    password_hash TEXT NOT NULL,
-    salt TEXT NOT NULL
+    password_hash TEXT NOT NULL
 );
