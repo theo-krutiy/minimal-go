b7fead437a8211595ae23b4a7251a8271600605e
Fedor Krutiy <theokrutij@yandex.ru>
2025-02-11 21:54:05 +0300

    auth middleware

---
+++ internal/server/middleware.go
@@ -0,0 +1,30 @@
+package server
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/theo-krutiy/minimal-go/internal/auth"
+)
+
+func (s *Server) RequiresLogin(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "", http.StatusUnauthorized)
+			return
+		}
+
+		if !strings.HasPrefix(authHeader, "Bearer ") {
+			http.Error(w, "", http.StatusUnauthorized)
+			return
+		}
+
+		token := strings.TrimPrefix(authHeader, "Bearer ")
+		if err := auth.ValidateToken(token, s.secret); err != nil {
+			http.Error(w, "", http.StatusUnauthorized)
+			return
+		}
+		next(w, r)
+	}
+}
