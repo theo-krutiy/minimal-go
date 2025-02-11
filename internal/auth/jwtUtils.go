c3319717fdf6b0b968dc460b3dd05e6801814ec5
Fedor Krutiy <theokrutij@yandex.ru>
2025-02-11 20:21:18 +0300

    jwt logic

--- internal/auth/jwtUtils.go
+++ internal/auth/jwtUtils.go
@@ -1,9 +1,37 @@
 package auth
 
-func issueNewJWT() (string, error) {
-	return "DummyJWT", nil
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func newClaims() jwt.Claims {
+	return jwt.RegisteredClaims{
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+	}
 }
 
-func validateJWT(token string) error {
-	return nil
+func issueNewJWT(secret []byte) (string, error) {
+	claims := newClaims()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	ss, err := token.SignedString(secret)
+	if err != nil {
+		return "", err
+	}
+	return ss, nil
+}
+
+func validateJWT(tokenString string, secret []byte) error {
+	var kf = func(token *jwt.Token) (interface{}, error) { return secret, nil }
+	token, err := jwt.Parse(tokenString, kf, jwt.WithExpirationRequired())
+	switch {
+	case token.Valid:
+		return nil
+	case errors.Is(err, jwt.ErrTokenExpired):
+		return errors.New("token expired")
+	default:
+		return errors.New("malformed token")
+	}
 }
