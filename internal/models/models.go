298ab32507421e597736095c7d7b8f8e059379f8
Fedor Krutiy <theokrutij@yandex.ru>
2025-02-13 00:10:51 +0300

    add db tags to models

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,15 +1,15 @@
 package models
 
 type UserInDatabase struct {
-	Id           string
-	Login        string
-	PasswordHash []byte
+	Id           string `db:"id"`
+	Login        string `db:"login"`
+	PasswordHash []byte `db:"password_hash"`
 }
 
 type ItemInDatabase struct {
-	Id             string `json:"id"`
-	Name           string `json:"name"`
-	CountAvailable int    `json:"count_available"`
-	PriceInteger   int    `json:"price_integer"`
-	PriceDecimal   int    `json:"price_decimal"`
+	Id             string `json:"id" db:"id"`
+	Name           string `json:"name" db:"name"`
+	CountAvailable int    `json:"count_available" db:"count_available"`
+	PriceInteger   int    `json:"price_integer" db:"price_integer"`
+	PriceDecimal   int    `json:"price_decimal" db:"price_decimal"`
 }
