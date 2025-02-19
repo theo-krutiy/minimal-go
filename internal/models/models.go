4878bb6232a74d26f12c816d1b9a36d9ce1b9857
Fedor Krutiy <theokrutij@yandex.ru>
2025-02-19 11:40:36 +0300

    add cart model

--- internal/models/models.go
+++ internal/models/models.go
@@ -13,3 +13,15 @@ type Item struct {
 	PriceInteger   int    `json:"price_integer" db:"price_integer"`
 	PriceDecimal   int    `json:"price_decimal" db:"price_decimal"`
 }
+
+type ItemInCart struct {
+	ItemId string `json:"item_id" db:"item_id"`
+	CartId string `json:"cart_id" db:"cart_id"`
+	Count  int    `json:"count" db:"count_in_cart"`
+}
+
+type Cart struct {
+	Id     string        `json:"id" db:"id"`
+	UserId string        `json:"user_id" db:"user_id"`
+	Items  []*ItemInCart `json:"items"`
+}
