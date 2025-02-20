e08e5873e2bbb9449dfd9b270a41a1ba4b5d1ed9
Fedor Krutiy <theokrutij@yandex.ru>
2025-02-20 17:42:02 +0300

    SQL SCHEMA: replace transactions with orders

--- postgres/init.sql
+++ postgres/init.sql
@@ -23,18 +23,18 @@ CREATE TABLE items_in_cart (
     CONSTRAINT unique_cart_item UNIQUE(item_id, cart_id)
 );
 
-CREATE TABLE transactions (
+CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
     user_id INT REFERENCES users(id) ON DELETE CASCADE,
-    status VARCHAR(64) NOT NULL,
+    total_price INT NOT NULL CHECK (total_price >= 0),
+    state VARCHAR(64) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     completed_at TIMESTAMP 
 );
 
-CREATE TABLE items_in_transaction (
-    transaction_id INT REFERENCES transactions(id) ON DELETE CASCADE,
-    item_id INT REFERENCES items(id) ON DELETE CASCADE,
-    count_in_transaction INT NOT NULL CHECK (count_in_transaction >= 0),
-    price_integer INT NOT NULL CHECK (price_integer >= 0),
-    price_decimal INT NOT NULL CHECK (price_decimal >= 0 AND price_decimal < 100)
+CREATE TABLE items_in_order (
+    item_id INT REFERENCES items(id) ON DELETE CASCADE,    
+    order_id INT REFERENCES orders(id) ON DELETE CASCADE,
+    count INT NOT NULL CHECK (count >= 0),
+    price INT NOT NULL CHECK (price >= 0),
 );
