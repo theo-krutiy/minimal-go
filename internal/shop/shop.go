e490517982956066ed1ce75b53c5d1cbe7a8ea2b
Fedor Krutiy <theokrutij@yandex.ru>
2025-02-28 00:52:33 +0300

    shop returns error codes

--- internal/shop/shop.go
+++ internal/shop/shop.go
@@ -2,7 +2,9 @@ package shop
 
 import (
 	"errors"
+	"fmt"
 
+	"github.com/theo-krutiy/minimal-go/internal/core"
 	"github.com/theo-krutiy/minimal-go/internal/models"
 )
 
@@ -16,37 +18,47 @@ type Database interface {
 
 func GetItems(query string, offset, limit int, db Database) (page []*models.Item, totalResults int, err error) {
 	if limit <= 0 {
-		err = errors.New("limit must be positive")
+		err = fmt.Errorf("limit must be positive%w", core.ErrValidation)
 		return
 	}
 	if offset < 0 {
-		err = errors.New("offset must not be negative")
+		err = fmt.Errorf("offset must not be negative%w", core.ErrValidation)
 		return
 	}
 
 	page, totalResults, err = db.ReadItems(query, offset, limit)
+	if err != nil {
+		err = core.ErrUnknown
+	}
 	return
 }
 
 func GetCart(userId string, db Database) (cart *models.Cart, err error) {
 	cart, err = db.GetCart(userId)
-	if err != nil {
-		return nil, err
+	switch {
+	case err == nil:
+		return cart, nil
+	case errors.Is(err, core.ErrDBNoData):
+		return nil, core.ErrNoData
+	default:
+		return nil, core.ErrUnknown
 	}
-	return cart, nil
 }
 
 func AddItemToCart(cartId, itemId string, countAdded *int, db Database) error {
 	switch {
 	case *countAdded <= 0:
-		return errors.New("count must be positive")
+		return fmt.Errorf("count must be positive%w", core.ErrValidation)
 	case countAdded == nil:
 		v := 1
 		countAdded = &v
 	}
 
 	err := db.IncrementItemInCart(cartId, itemId, *countAdded)
-	return err
+	if err != nil {
+		return core.ErrUnknown
+	}
+	return nil
 }
 
 func RemoveItemFromCart(cartId, itemId string, countRemoved *int, db Database) error {
@@ -59,18 +71,24 @@ func RemoveItemFromCart(cartId, itemId string, countRemoved *int, db Database) e
 	}
 
 	err := db.IncrementItemInCart(cartId, itemId, *countRemoved*-1)
-	return err
+	if err != nil {
+		return core.ErrUnknown
+	}
+	return nil
 }
 
 func EmptyCart(cartId string, db Database) error {
 	err := db.EmptyCart(cartId)
-	return err
+	if err != nil {
+		return core.ErrUnknown
+	}
+	return nil
 }
 
 func ConvertCartIntoOrder(cartId string, db Database) (string, error) {
 	orderId, err := db.ConvertCartIntoOrder(cartId)
 	if err != nil {
-		return "", err
+		return "", core.ErrUnknown
 	}
 
 	return orderId, nil
